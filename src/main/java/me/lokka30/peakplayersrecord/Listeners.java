7d2ab93f2d02e0bfba2cf8440d95c44d0f4cba06
lokka30 <lokka30@protonmail.com>
2021-01-03 10:38:31 +0800

    v1.0.0

---
+++ src/main/java/me/lokka30/peakplayersrecord/Listeners.java
@@ -0,0 +1,50 @@
+package me.lokka30.peakplayersrecord;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listeners implements Listener {
+
+    /**
+     * Access to the main class.
+     */
+    private final PeakPlayersRecord instance;
+    public Listeners(final PeakPlayersRecord instance) { this.instance = instance; }
+
+    @EventHandler
+    public void onJoin(final PlayerJoinEvent event) {
+        instance.debugMessage("A player has joined.");
+
+        final int currentlyOnline = Bukkit.getOnlinePlayers().size();
+
+        if (currentlyOnline > instance.currentPeak) {
+            // Change peak count.
+            instance.currentPeak = currentlyOnline;
+            instance.getConfig().set("data.currentPeak", instance.currentPeak);
+
+            // Set player list.
+            final ArrayList<String> usernamesOnline = new ArrayList<>();
+            Bukkit.getOnlinePlayers().forEach(onlinePlayer -> usernamesOnline.add(onlinePlayer.getName()));
+            instance.getConfig().set("data.playerListAtPeak", String.join(", ", usernamesOnline));
+
+            // Log the achievement.
+            instance.getLogger().info("New peak player count reached: " + instance.currentPeak + " players are online.");
+
+            // Announce the achievement.
+            if (instance.getConfig().getBoolean("messages.announcement.enabled")) {
+                List<String> messages = instance.getConfig().getStringList("messages.announcement.text");
+                messages.forEach(message -> Bukkit.broadcastMessage(instance.colorize(message).replace("%players%", instance.currentPeak + "")));
+            }
+
+            // Write changes to the disk
+            instance.saveConfig();
+
+            instance.debugMessage("Finished peak code.");
+        }
+    }
+}
--- src/main/java/me/lokka30/peakplayersrecord/PeakPlayersRecord.java
+++ src/main/java/me/lokka30/peakplayersrecord/PeakPlayersRecord.java
@@ -1,116 +1,55 @@
 package me.lokka30.peakplayersrecord;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 public class PeakPlayersRecord extends JavaPlugin {
 
-    int currentPeak;
+    /**
+     * This is the number of players of the highest peak, it is stored in memory to minimise disk usage.
+     */
+    public int currentPeak;
 
+    /**
+     * This is ran when the plugin starts up.
+     */
     @Override
     public void onEnable() {
         // Load config + set appropriate options
+        getLogger().info("Loading config...");
         saveDefaultConfig();
         getConfig().options().copyDefaults(true);
         getConfig().options().header("PeakPlayersRecord by lokka30");
 
         // Save the peak in memory to minimise disk usage
+        getLogger().info("Storing current peak to memory...");
         currentPeak = getConfig().getInt("data.currentPeak");
 
         // Register the listener to put the plugin into effect
-        getServer().getPluginManager().registerEvents(new Listeners(), this);
+        getLogger().info("Registering listeners...");
+        getServer().getPluginManager().registerEvents(new Listeners(this), this);
 
         // Register the command
-        Objects.requireNonNull(getCommand("peakplayersrecord")).setExecutor(new Commands());
+        getLogger().info("Registering commands...");
+        Objects.requireNonNull(getCommand("peakplayersrecord")).setExecutor(new Commands(this));
+
+        getLogger().info("Start-up complete.");
     }
 
-    private String colorize(final String msg) {
+    /**
+     * Translates all standard Minecraft colour codes (e.g. &a = lime green) in the specified message.
+     * @param msg the message to translate
+     * @return the translated message
+     */
+    public String colorize(final String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
-    class Listeners implements Listener {
-        @EventHandler
-        public void onJoin(final PlayerJoinEvent event) {
-            final int currentlyOnline = Bukkit.getOnlinePlayers().size();
-
-            if (currentlyOnline > currentPeak) {
-                // Change peak count.
-                currentPeak = currentlyOnline;
-                getConfig().set("data.currentPeak", currentPeak);
-
-                // Set player list.
-                final ArrayList<String> usernamesOnline = new ArrayList<>();
-                Bukkit.getOnlinePlayers().forEach(onlinePlayer -> usernamesOnline.add(onlinePlayer.getName()));
-                getConfig().set("data.playerListAtPeak", String.join(",", usernamesOnline));
-
-                // Log the achievement.
-                getLogger().info("New peak player count reached: " + currentPeak + " players are online.");
-
-                // Announce the achievement.
-                if (getConfig().getBoolean("messages.announcement.enabled")) {
-                    List<String> messages = getConfig().getStringList("messages.announcement.text");
-                    messages.forEach(message -> {
-                        message = colorize(message);
-                        message = message.replace("%players%", currentPeak + "");
-                        Bukkit.broadcastMessage(message);
-                    });
-                }
-
-                // Write changes to the disk
-                saveConfig();
-            }
-        }
-    }
-
-    class Commands implements TabExecutor {
-
-        @Override
-        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if (sender.hasPermission("peakplayersrecord.command")) {
-                if (args.length == 0) {
-                    getConfig().getStringList("messages.help").forEach(sender::sendMessage);
-                } else if (args.length == 1) {
-                    if (args[0].equalsIgnoreCase("reload")) {
-                        if (sender.hasPermission("peakplayersrecord.command.reload")) {
-                            getConfig().getStringList("messages.reload.start").forEach(sender::sendMessage);
-
-                            saveDefaultConfig(); // In case they've decided to delete it whilst the plugin is running.
-                            reloadConfig(); // Reload the config now
-
-                            getConfig().getStringList("messages.reload.finish").forEach(sender::sendMessage);
-                        } else {
-                            getConfig().getStringList("messages.no-permission").forEach(sender::sendMessage);
-                        }
-                    } else {
-                        getConfig().getStringList("messages.usage").forEach(sender::sendMessage);
-                    }
-                } else {
-                    getConfig().getStringList("messages.usage").forEach(sender::sendMessage);
-                }
-            } else {
-                getConfig().getStringList("messages.no-permission").forEach(sender::sendMessage);
-            }
-            return true;
-        }
-
-        @Override
-        public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-            if (args.length == 0) {
-                return Collections.singletonList("reload");
-            }
-            return null;
+    public void debugMessage(final String msg) {
+        if(getConfig().getBoolean("debug")) {
+            getLogger().info(colorize("&9[DEBUG] &7" + msg));
         }
     }
 }
